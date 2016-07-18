5434c0f8d62d3a2c1bd613b36c922a75f8f703ad
thaihoc1994 <bboyfool215@gmail.com>
2016-07-18 23:39:24 +0700

    Check Boxx

---
+++ LearnCheckBox/app/src/main/java/com/example/thaihoc/learncheckbox/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.thaihoc.learncheckbox;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    CheckBox chkAndroi, chkWP, chkIOS;
+    Button btnChon;
+    TextView txtMonHoc;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+        addControl();
+        addEvent();
+    }
+
+    private void addEvent() {
+        btnChon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                xuLyChonMonHoc();
+            }
+        });
+    }
+
+    private void xuLyChonMonHoc() {
+        String s = "";
+        if (chkAndroi.isChecked())
+            s+=chkAndroi.getText().toString();
+    }
+
+    private void addControl() {
+        chkAndroi = (CheckBox) findViewById(R.id.chkAndroid);
+        chkWP = (CheckBox) findViewById(R.id.chkWP);
+        chkIOS = (CheckBox) findViewById(R.id.chkIOS);
+        btnChon = (Button) findViewById(R.id.chkAndroid);
+        txtMonHoc = (TextView) findViewById(R.id.txtMonhoc);
+    }
+}
